696504cbe3386ca8dc629484071d8565fdef6204
David Sherret <dsherret@gmail.com>
2022-03-23 19:30:10 -0400

    Use automation repo script for creating plugin file.

--- scripts/createPluginFile.ts
+++ scripts/createPluginFile.ts
@@ -1,45 +1,17 @@
-const packageText = Deno.readTextFileSync("Cargo.toml");
-// version = "x.x.x"
-const version = packageText.match(/version\s*=\s*\"(\d+\.\d+\.\d+)\"/)![1];
-const pluginName = "dprint-plugin-exec";
+import * as path from "https://deno.land/std@0.130.0/path/mod.ts";
+import { extractCargoVersion, processPlugin } from "https://raw.githubusercontent.com/dprint/automation/0.2.0/mod.ts";
 
-if (!/^\d+\.\d+\.\d+$/.test(version)) {
-  throw new Error("Error extracting version.");
-}
+const currentDirPath = path.dirname(path.fromFileUrl(import.meta.url));
+const cargoFilePath = path.join(currentDirPath, "../", "Cargo.toml");
 
-const outputFile = {
-  schemaVersion: 2,
-  name: pluginName,
-  version,
-  "darwin-x86_64": await getPlatformObject(`${pluginName}-x86_64-apple-darwin.zip`),
-  "darwin-aarch64": await getPlatformObject(`${pluginName}-aarch64-apple-darwin.zip`),
-  "linux-x86_64": await getPlatformObject(`${pluginName}-x86_64-unknown-linux-gnu.zip`),
-  "windows-x86_64": await getPlatformObject(`${pluginName}-x86_64-pc-windows-msvc.zip`),
-};
-Deno.writeTextFile("plugin.exe-plugin", JSON.stringify(outputFile, undefined, 2) + "\n");
-
-async function getPlatformObject(zipFileName: string) {
-  const fileBytes = Deno.readFileSync(zipFileName);
-  const checksum = await getChecksum(fileBytes);
-  console.log(zipFileName + ": " + checksum);
-  return {
-    "reference": getPlatformReferenceUrl(zipFileName),
-    "checksum": checksum,
-  };
-}
-
-async function getChecksum(bytes: Uint8Array) {
-  // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
-  const hashBuffer = await crypto.subtle.digest("SHA-256", bytes);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
-  return hashHex;
-}
-
-function getPlatformReferenceUrl(zipFileName: string) {
-  if (Deno.args.some(arg => arg === "--test")) {
-    return zipFileName;
-  } else {
-    return `https://github.com/dprint/${pluginName}/releases/download/${version}/${zipFileName}`
-  }
-}
+await processPlugin.createDprintOrgProcessPlugin({
+  pluginName: "dprint-plugin-exec",
+  version: await extractCargoVersion(cargoFilePath),
+  platforms: [
+    "darwin-aarch64",
+    "darwin-x86_64",
+    "linux-x86_64",
+    "windows-x86_64",
+  ],
+  isTest: Deno.args.some(a => a == "--test"),
+});

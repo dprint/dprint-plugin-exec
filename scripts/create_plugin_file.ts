0626ba85e49221f15fb93c8210b8a3382c72a57c
David Sherret <dsherret@gmail.com>
2022-07-01 13:26:19 -0400

    0.3.1

--- scripts/create_plugin_file.ts
+++ scripts/create_plugin_file.ts
@@ -1,5 +1,5 @@
-import * as path from "https://deno.land/std@0.130.0/path/mod.ts";
-import { extractCargoVersion, processPlugin } from "https://raw.githubusercontent.com/dprint/automation/0.3.0/mod.ts";
+import * as path from "https://deno.land/std@0.146.0/path/mod.ts";
+import { extractCargoVersion, processPlugin } from "https://raw.githubusercontent.com/dprint/automation/0.4.0/mod.ts";
 
 const currentDirPath = path.dirname(path.fromFileUrl(import.meta.url));
 const cargoFilePath = path.join(currentDirPath, "../", "Cargo.toml");
@@ -10,8 +10,8 @@ await processPlugin.createDprintOrgProcessPlugin({
   platforms: [
     "darwin-aarch64",
     "darwin-x86_64",
-    "linux-x86_64",
     "linux-aarch64",
+    "linux-x86_64",
     "windows-x86_64",
   ],
   isTest: Deno.args.some(a => a == "--test"),

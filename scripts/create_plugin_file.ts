8ac178d3fb04c89368364bc922745fc5a880d990
David Sherret <dsherret@users.noreply.github.com>
2022-12-17 16:45:38 -0500

    chore: fix musl support (#19)

--- scripts/create_plugin_file.ts
+++ scripts/create_plugin_file.ts
@@ -1,5 +1,5 @@
 import * as path from "https://deno.land/std@0.153.0/path/mod.ts";
-import { extractCargoVersionOrThrow, processPlugin } from "https://raw.githubusercontent.com/dprint/automation/0.5.3/mod.ts";
+import { extractCargoVersionOrThrow, processPlugin } from "https://raw.githubusercontent.com/dprint/automation/0.6.0/mod.ts";
 
 const currentDirPath = path.dirname(path.fromFileUrl(import.meta.url));
 const cargoFilePath = path.join(currentDirPath, "../", "Cargo.toml");

d3546e3b9ec44f224311245b20f4a1b483c68066
David Sherret <dsherret@gmail.com>
2023-10-28 15:33:38 -0400

    Try removing cc

--- .github/workflows/ci.generate.ts
+++ .github/workflows/ci.generate.ts
@@ -129,7 +129,6 @@ const ci = {
           if: "!startsWith(github.ref, 'refs/tags/')",
           env: {
             "CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER": "aarch64-linux-gnu-gcc",
-            "CC_aarch64_unknown_linux_musl": "aarch64-linux-gnu-gcc",
           },
           run: "cargo build --locked --all-targets --target ${{matrix.config.target}}",
         },
@@ -138,7 +137,6 @@ const ci = {
           if: "startsWith(github.ref, 'refs/tags/')",
           env: {
             "CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER": "aarch64-linux-gnu-gcc",
-            "CC_aarch64_unknown_linux_musl": "aarch64-linux-gnu-gcc",
           },
           run: "cargo build --locked --all-targets --target ${{matrix.config.target}} --release",
         },
